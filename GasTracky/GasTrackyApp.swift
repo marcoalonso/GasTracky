11f09fedd782a4a697b3ac16ea14dffa2770c66f
Marco Alonso <marcoalonsorota@hotmail.com>
2024-11-04 12:22:24 -0600

    Initial Commit

---
+++ GasTracky/GasTrackyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GasTrackyApp.swift
+//  GasTracky
+//
+//  Created by Marco Alonso on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GasTrackyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
